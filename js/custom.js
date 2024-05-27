d436f9da6db35b5a2f49bf8250ab88310fa72cec
auskren <kbberkuk@gmail.com>
2024-05-27 22:38:03 +0300

    Added dynamic post requests with ajax

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 
+
 // This function shows sent form input on client side for confirmation 
 function showReq(){
   alert(
@@ -19,11 +20,59 @@ function showReq(){
   $("#date").val("");
 }
 
+function fetchBlogs(page, path) {
+  const host = window.location.hostname;
+  const port = location.port
+  console.log("page: ", page)
+  const url = `http://${host}:${port}/content/${path}/${path}${page}.json`;
+
+  $.ajax({
+      url: url,
+      method: 'GET',
+      success: function(data) {
+          console.log("data is ", data.post)
+          const posts = data.posts;
+          const post_list = $('.post-list');
+          post_list.empty()
+          posts.forEach(post => {
+              post_list.append(`
+                  <div class="post">
+                    <img src="${post.photo}" alt="${post.title}" class="post-image" />
+                    <div class="post-content">
+                      <h2 class="post-title">${post.title}</h2>
+                      <p class="post-date">${post.date}</p>
+                      <p class="post-excerpt">
+                        ${post.excerpt}
+                      </p>
+                    </div>
+                  </div>
+              `);
+          });
+      },
+      error: function(error) {
+          console.log('Error fetching blogs:', error);
+      }
+  });
+}
+
+let page = 1
+
+function pageUp(page,path){
+  page+=1
+  fetchBlogs(page,path)
+  return page
+}
+
+function pageDown(page,path){
+  page-=1
+  fetchBlogs(page,path)
+  return page
+}
+
 
 
 $(document).ready(function () { 
-    
-
+  
 
   /** Initiate validation on each form with class .contant-form */ 
   $('.contact-form').validation({ 
@@ -43,9 +92,7 @@ $(document).ready(function () {
         'position': 'right', // Position of the navigation bar
         'tooltips': ['Home', 'Blog', 'Projects', 'Contact', 'About Us'] // Tooltips for navigation items
     }
-  });
-
-  
+  });  
   
 })
 
